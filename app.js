d790c67037482cd8e83a189367d7e58322d7e1a7
JEIPER <Pedrodrask16@gmail.com>
2025-09-13 01:09:41 -0500

    feat: implementar función agregarAmigo con validación y push al array

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 // Guardamos los nombres de los amigos
-let  AmigoSecreto = [];
+let  amigos = [];
 
 
 // Función para agregar un nuevo amigo
-function AmigoSecreto() {
+function agregaramigos() {
     const input = document.getElementById("amigo");
     const nombre = input.value.trim();
 
